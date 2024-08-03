37cf2d34e6d0c0bdbeeafd395b1c211949886ba4
Henrique Higa <codehiga@gmail.com>
2024-08-03 14:30:00 -0300

    feat: adicionado link no medu

--- src/app/components/menu-lateral/menu-lateral.component.ts
+++ src/app/components/menu-lateral/menu-lateral.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu-lateral',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './menu-lateral.component.html',
   styleUrl: './menu-lateral.component.scss'
 })
